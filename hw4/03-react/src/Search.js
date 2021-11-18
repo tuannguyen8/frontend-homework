4142b4024cb768eb10d6a8e508535b3ce8adefcf
Tuan Nguyen <nguyenvutuan1@gmail.com>
2021-11-18 12:48:02 -0800

    react hw

--- hw4/03-react/src/Search.js
+++ hw4/03-react/src/Search.js
@@ -6,70 +6,58 @@ class SearchPage extends Component{
   constructor(props){
     super(props)
     this.state ={
-      characterId: 41,
-      data: []
+      characterId: -1,
+      data: {},
+      tempCharacter: ""
       
     }
   }
-  handleDelete = (event) =>{
 
-  }
   handleSubmit = (event)=>{
     event.preventDefault(); 
-    const{characterId} = this.state
-    console.log("characterId: ", characterId)
-    this.setState({characterId});
-  }
-  handleSubmitChange = (event)=>{
-    event.preventDefault();
-    /* console.log(event)*/
-   /*  console.log("name event: ", event.target.name)
-    console.log("value event: ",event.target.value) */ 
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
-  
-  componentDidMount(){ 
-    //let characterId = 2;
-    const{characterId} = this.state
-    console.log("alo: ",characterId)
-    fetch(`https://thronesapi.com/api/v2/Characters/${characterId}`)
+
+    let id = this.state.tempCharacter
+    
+    fetch(`https://thronesapi.com/api/v2/Characters/${id}`)
       .then(response => response.json())
       .then(data => {
-        //console.log(data);
-        this.setState({data})
+        console.log(data)
+        this.setState({characterId: Number(id)})
+        this.setState({data: data})
+      })
+      .catch(error => {
+        console.log(error)
       });
-    
+  }
+
+
+  handleChange =(event) =>{
+    this.setState({
+      tempCharacter: event.target.value
+    })
   }
 
   render(){
-    
-    const{characterId} = this.state
-    const {data} = this.state;
-
     return (
       <div className="Content-Search">
         <h1>Thrones Characters</h1>
-        <p>Character Id is: {characterId}</p>
-        <form onSubmit={this.handleDelete, this.handleSubmit}>
+        <form onSubmit={ this.handleSubmit}>
           <label>
             Character ID:
-            <input type="number" placeholder="Id of Chacracter" name = "characterId" onChange={this.handleSubmitChange}/>
+          <input onChange={this.handleChange} type="number" value={this.state.tempCharacter} placeholder="Id of Chacracter" name = "characterId"/>
           </label>
           <button>Submit</button>
         </form> 
-        <div className="Card">
-          <img src={data.imageUrl} alt="character-img"></img> 
-          <div className="Character-Info">
-            <p>Character id: {data.id}</p>
-            <p>Full name: {data.fullName}</p>
-            <p>Title: {data.title}</p>
-          </div>
-          
-        </div>
-        
-        
+        {this.state.tempCharacter !== "" &&
+          <div className="Card">
+            <img src={this.state.data.imageUrl} alt="character-img"></img> 
+            <div className="Character-Info">
+              <p>Character id: {this.state.data.id}</p>
+              <p>Full name: {this.state.data.fullName}</p>
+              <p>Title: {this.state.data.title}</p>
+            </div>
+          </div>        
+        }  
       </div>
     );
  }
